05537aef379e55a7a428670a85a00a82a7e374e2
asher <asher@breathings.co.kr>
2022-08-03 10:45:03 +0900

    d3 test code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+import { select } from 'd3-selection';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([24, 30, 45, 70, 26]);
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    svg
+      .selectAll("cicle")
+      .data(data)
+      .join(
+        (enter) => enter.append("circle"),
+        (update) => update.attr("class", "updated"),
+        (exit) => exit.remove()
+      )
+      .attr("r", (value) => value)
+      .attr("cx", (value) => value * 2)
+      .attr("cy", (value) => value * 3)
+      .attr("stroke", "red");
+
+  }, [data])
+  const removeAll = () => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .remove();
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg ref={svgRef} style={{width : '100%', height : '90%'}}/>
+      <button type="button" onClick={() => {setData(data.map(el => el + 5))}}>Increase + 5</button>
+      <button type="button" onClick={() => {setData(data.filter(el => el > 35))}}>Filter circle r should gt 35</button>
+      <button type="button" onClick={removeAll}>Delete</button>
     </div>
   );
 }
